ed2e57dbbac8c69464e94c20d41d408d85076256
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-01-13 14:45:32 +0530

    complete create user component

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react"
 
-function CreateUser() {
-  return <div>Create User</div>;
+function CreateUser(props) {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+
+  // check if valid or invalid
+  const isValid = () => {
+    return name && email
+  }
+
+  const isInvalid = () => {
+    return !isValid()
+  }
+
+  // form submit handler
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    if (props.onCreateUser) {
+      props.onCreateUser({
+        name,
+        email
+      })
+    }
+
+    setName("")
+    setEmail("")
+  }
+  return (
+    <form className="CreateUser" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={event => setName(event.target.value)}
+        placeholder="Name"
+      />
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={event => setEmail(event.target.value)}
+        placeholder="email"
+      />
+      <input type="submit" value="Create User" disabled={isInvalid()} />
+    </form>
+  )
 }
 
-export default CreateUser;
+export default CreateUser
