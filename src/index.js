8655c78a375366812fe10865f31b40452d103090
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-01-13 14:45:01 +0530

    add extra param to enable redux dev tools

--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ import Application from "./components/Application"
 import "./index.scss"
 
 // create redux store
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <Provider store={store}>
