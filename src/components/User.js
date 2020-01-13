c1de3f58126d88c5234ebe250699beeb3e1669ff
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-01-13 14:49:43 +0530

    complete user component

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React from "react"
+import md5 from "md5"
 
-function User() {
-  return <div>User</div>;
+// create profile image URL
+const createProfileImageURL = ({ email }) => {
+  const hash = md5(email.trim())
+
+  return `https://wwww.gravatar.com/avatar/${hash}`
 }
 
-export default User;
+function User({ user }) {
+  const profileImage = createProfileImageURL(user)
+
+  return (
+    <article className="User">
+      <img className="User-picture" src={profileImage} alt={user.name} />
+      <div className="User-info">
+        <h2>{user.name}</h2>
+      </div>
+    </article>
+  )
+}
+
+export default User
