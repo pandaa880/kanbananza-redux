2fc776d2d58fdec5baee0936fb4a8a7aff23c652
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-01-17 13:29:28 +0530

    add user reducer in combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux"
 
 import lists from "./list-reducer"
 import cards from "./card-reducer"
+import users from "./user-reducer"
 
 export default combineReducers({
   lists,
-  cards
+  cards,
+  users
 })
