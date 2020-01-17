952250b73d094e9f71845b1f4bb296b710b078d4
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-01-17 13:44:06 +0530

    create UserContainer to get access to particular user object from redux store

---
+++ src/containers/UserContainer.js
@@ -0,0 +1,11 @@
+import { connect } from "react-redux"
+
+import User from "../components/User"
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.users.entities[ownProps.userId]
+  }
+}
+
+export default connect(mapStateToProps)(User)
