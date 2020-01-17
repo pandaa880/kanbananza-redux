62dccdc5f88bd80a86870849020cc01c160b6fbf
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-01-17 13:39:27 +0530

    create users container to connect users from redux store to users component

---
+++ src/containers/UsersContainer.js
@@ -0,0 +1,11 @@
+import { connect } from "react-redux"
+
+import Users from "../components/Users"
+
+const mapStateToProps = state => {
+  return {
+    users: state.users.ids
+  }
+}
+
+export default connect(mapStateToProps)(Users)
