106f75fa91daf675e58a7c5d0c76cabf40d59797
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-01-19 16:55:19 +0530

    complete component to render lists dropdown and add functionality to change list

--- src/components/MoveCardToList.js
+++ src/components/MoveCardToList.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
 
-function MoveCardToList() {
-  return <div>Move Card To List</div>;
+function MoveCardToList({ cardId, lists, listId, moveCardToList }) {
+  // lists
+  const [listsArr, setListsArr] = useState(Object.values(lists.entities))
+  // currently selected list id
+  const [selectedListId, setSelectedListId] = useState(listId)
+
+  // componentdidupdate call
+  useEffect(() => {
+    setListsArr(Object.values(lists.entities))
+  }, [lists])
+
+  // handle when user changes the list by selecting a new list name
+  const handleDropdownChange = e => {
+    const newSelectedListId = e.target.value
+    setSelectedListId(newSelectedListId)
+    moveCardToList(cardId, listId, newSelectedListId, lists)
+  }
+  return (
+    <select
+      className="Card-move"
+      onChange={handleDropdownChange}
+      value={selectedListId}
+    >
+      {listsArr &&
+        listsArr.map(list => (
+          <option value={list.id} key={list.id}>
+            {list.title}
+          </option>
+        ))}
+    </select>
+  )
 }
 
-export default MoveCardToList;
+export default MoveCardToList
