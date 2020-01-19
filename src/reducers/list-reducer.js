a05e19b4cb28ad4b933b2c15b82acdcfb1c002ef
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-01-19 16:58:02 +0530

    add new action to manage move card to new list

--- src/reducers/list-reducer.js
+++ src/reducers/list-reducer.js
@@ -4,7 +4,7 @@ import { lists as defaultLists } from "../normalized-state"
 
 // card actions
 import { CREATE_LIST } from "../actions/list-actions"
-import { CREATE_CARD } from "../actions/card-actions"
+import { CREATE_CARD, MOVE_CARD_TO_LIST } from "../actions/card-actions"
 
 const listReducer = (lists = defaultLists, action) => {
   const actionType = action.type
@@ -28,6 +28,55 @@ const listReducer = (lists = defaultLists, action) => {
       ids: newListsIds
     }
   }
+
+  if (actionType === MOVE_CARD_TO_LIST) {
+    const { cardId, currentListId, newListId, list } = actionPayload
+
+    // remove card from currentList
+    const currentListCards = [...list.entities[currentListId].cards]
+    const newListCards = [...list.entities[newListId].cards]
+
+    const cardIndexInCurrentList = currentListCards.findIndex(
+      element => element === cardId
+    )
+
+    if (cardIndexInCurrentList >= 0) {
+      currentListCards.splice(cardIndexInCurrentList, 1)
+
+      // add new card id in selected list
+      newListCards.push(cardId)
+
+      // update current list with previous card removed
+      const updatedCurrentList = {
+        ...list.entities[currentListId],
+        cards: currentListCards
+      }
+
+      // updated new list with new card
+      const updatedNewList = {
+        ...list.entities[newListId],
+        cards: newListCards
+      }
+
+      // construct new list entities
+      const newListEntities = {
+        ...list.entities,
+        [currentListId]: updatedCurrentList,
+        [newListId]: updatedNewList
+      }
+
+      return {
+        entities: newListEntities,
+        ids: list.ids
+      }
+    }
+
+    return {
+      entities: { ...list.entities },
+      ids: list.ids
+    }
+  }
+
   return lists
 }
 
