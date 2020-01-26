763ae323dfa4f9546de7f51c968e131be6d858ae
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-01-26 17:03:55 +0530

    import and render RemoveListButton component and pass required props

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,20 @@
 import React from "react"
 
+import RemoveListButton from "./RemoveListButton"
 import CreateCardContainer from "../containers/CreateCardContainer"
 import CardContainer from "../containers/CardContainer"
 
-function List({ list = {}, removeList }) {
+function List({ list = {}, listId, lists, removeList }) {
   return (
     <article className="List">
-      <h2>{list.title}</h2>
+      <div className="list-title-and-delete-btn-container">
+        <h2>{list.title}</h2>
+        <RemoveListButton
+          listId={listId}
+          lists={lists}
+          removeList={removeList}
+        />
+      </div>
       <CreateCardContainer listId={list.id} />
       <div>
         {list.cards.map(cardId => (
