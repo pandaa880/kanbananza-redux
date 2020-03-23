e15d5ac420a17d6b910a1bf33027aaf5ccb49d27
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-03-23 18:38:42 +0530

    add reselect for performance

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,10 +1,17 @@
 import { connect } from "react-redux"
+import { createSelector } from "reselect"
 
 import Users from "../components/Users"
 
+const getUsers = users => {
+  return users.ids
+}
+
+const createUserSelector = createSelector(state => state.users, getUsers)
+
 const mapStateToProps = state => {
   return {
-    users: state.users.ids
+    users: createUserSelector(state)
   }
 }
 
