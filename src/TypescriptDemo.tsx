39a3835c6cde85a41c8248afa588fc6066719bf3
Prashant <prashant.chaudhari@simpplr.com>
2021-10-29 13:03:52 +0530

    add typescript support and fix npm security vulnerabilities

---
+++ src/TypescriptDemo.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function TypescriptDemo() {
+  return (
+    <div>
+      <h1>Hello from TypeScript !</h1>
+    </div>
+  )
+}
+
+export default TypescriptDemo
--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,22 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { createStore } from "redux"
 import { Provider } from "react-redux"
+import { composeWithDevTools } from "redux-devtools-extension"
 
 // import reducer
 import rootReducer from "./reducers"
 
 import Application from "./components/Application"
+import TSDemo from "./TypescriptDemo"
 
 import "./index.scss"
 
 // create redux store
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const store = createStore(rootReducer, composeWithDevTools())
 
 ReactDOM.render(
   <Provider store={store}>
+    <TSDemo />
     <Application />
   </Provider>,
   document.getElementById("root")
